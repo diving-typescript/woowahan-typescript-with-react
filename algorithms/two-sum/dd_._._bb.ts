0250c0d14d5690f9e861812842402686977d6a2e
B <function.hola.b@gmail.com>
2025-01-14 09:18:06 +0900

    feat: two sum

---
+++ algorithms/two-sum/dd_._._bb.ts
@@ -0,0 +1,11 @@
+function twoSum(nums, target) {
+    const numMap = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complement in numMap) return [numMap[complement], i];
+        numMap[nums[i]] = i;
+    }
+
+    return [];
+}
