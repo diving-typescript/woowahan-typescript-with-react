1591b52d9d340e15381778ddfb460a6a9921d039
B <function.hola.b@gmail.com>
2025-01-14 09:33:32 +0900

    refactor: {} to Map

--- algorithms/two-sum/dd_._._bb.ts
+++ algorithms/two-sum/dd_._._bb.ts
@@ -1,11 +1,11 @@
 function twoSum(nums, target) {
-    const numMap = {};
-
+    const numMap = new Map();
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
-        if (complement in numMap) return [numMap[complement], i];
-        numMap[nums[i]] = i;
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
     }
-
     return [];
 }
